e3ade5d53aecc25fbfb22d9cfcd6c7f6e8801606
bendikjohansen <bendikj@gmail.com>
2021-10-28 21:48:18 +0200

    Solve 54

--- 54.py
+++ 54.py
@@ -1,16 +1,8 @@
 from collections import Counter
 
 file = open('54.txt', 'r')
-plays = [[rows.strip().split(' ')[:5], rows.strip().split(' ')[5:]] for rows in file.readlines()]
-
-def values_per_suit(hand):
-    return [
-        [to_numerical(card[0]) for card in hand if card[1] == 'H'],
-        [to_numerical(card[0]) for card in hand if card[1] == 'D'],
-        [to_numerical(card[0]) for card in hand if card[1] == 'S'],
-        [to_numerical(card[0]) for card in hand if card[1] == 'C']
-    ]
-
+plays = [[row.strip().split(' ')[:5], row.strip().split(' ')[5:]] for row in file.readlines()]
+def values_per_suit(hand): return [[to_numerical(card[0]) for card in hand if card[1] == suit] for suit in 'CHSD']
 
 def to_numerical(value):
     if value == 'A': return 14
@@ -20,59 +12,64 @@ def to_numerical(value):
     if value == 'T': return 10
     return int(value)
 
+def royal_flush(values): return any([set(suit) == set(range(10,15)) for suit in values]), 1
 
-def has_royal_flush(values):
-    print(values)
-    return any([set(suit) == set(range(10,15)) for suit in values])
+def straight_flush(values):
+    return any([[v - min(suit) for v in sorted(suit)] == [0,1,2,3,4] for suit in values]), 1
 
+def four_of_a_kind(values):
+    most_common = Counter([value for suit in values for value in suit]).most_common()[0]
+    return most_common[1] == 4, most_common[0]
 
-def has_straight_flush(values):
-    for suit in values:
-        if [v - min(suit) for v in suit] == [0,1,2,3,4]:
-            return True
-    return False
+def full_house(values):
+    most_common = Counter([value for suit in values for value in suit]).most_common()
+    return most_common[0][1] == 3 and most_common[1][1] == 2, most_common[0][0]
 
+def flush(values):
+    return any([len(suit) == 5 for suit in values]), 1
 
-def has_four_of_a_kind(values):
+def straight(values):
+    flat_values = sorted([value for suit in values for value in suit])
+    return [v - min(flat_values) for v in flat_values] == [0,1,2,3,4], 1
+
+def three_of_a_kind(values):
     counted_values = Counter([value for suit in values for value in suit])
     value, count = counted_values.most_common()[0]
-    print(value, count)
-    return count == 4, value
+    return count == 3, value
 
+def two_pairs(values):
+    most_common = Counter([value for suit in values for value in suit]).most_common()
+    return len(most_common) >= 2 and most_common[0][1] == 2 and most_common[1][1] == 2, max(most_common[0][0], most_common[1][0])
 
-def has_full_house(values):
-    counted_values = Counter([value for suit in values for value in suit])
-    return counted_values.most_common()[0][1] >= 2 and any([len(suit) >= 3 for suit in values])
+def one_pair(values):
+    most_common = Counter([value for suit in values for value in suit]).most_common()[0]
+    return most_common[1] == 2, most_common[0]
 
-
-def has_flush(values):
-    return any([len(suit) == 5 for suit in values])
-
-
-def has_straight(values):
-    flat_values = [value for suit in values for value in suit]
-    print
-    return [v - min(flat_values) for v in flat_values] == [0,1,2,3,4]
+def evaluate_highest_cards(hand):
+    values = values_per_suit(hand)
+    return sum([v * 15**i for i, v in enumerate(list(sorted([value for suit in values for value in suit])))])
 
 def evaluate_hand(hand):
     values = values_per_suit(hand)
+    checks = [royal_flush, straight_flush, four_of_a_kind, full_house, flush, straight, three_of_a_kind, two_pairs, one_pair]
+    for index, check in enumerate(checks):
+        valid, score = check(values)
+        if valid:
+            return 15**(len(checks)-index) * score
+    return 0
 
-    properties = []
-    if has_royal_flush(values):
-        properties.append('royal flush')
-    if has_straight_flush(values):
-        properties.append('straight flush')
-    four_of_a_kind, value = has_four_of_a_kind(values)
-    if four_of_a_kind:
-        properties.append(('four of a kind', value))
-    if has_full_house(values):
-        properties.append('full house')
-    if has_flush(values):
-        properties.append('flush')
-    if has_straight(values):
-        properties.append('straight')
-    
-    return properties
+def evaluate_play(first_hand, second_hand):
+    first_eval, second_eval = evaluate_hand(first_hand), evaluate_hand(second_hand)
 
+    if first_eval > second_eval:
+        return 1
+    if second_eval > first_eval:
+        return 0
 
-print(evaluate_hand(['TS', 'TH', 'TC', 'TD', 'AS']))
+    first_highest, second_highest = evaluate_highest_cards(first_hand), evaluate_highest_cards(second_hand)
+    if first_highest > second_highest:
+        return 1
+    if second_highest > first_highest:
+        return 0
+
+print(sum([evaluate_play(*play) for play in plays]))
